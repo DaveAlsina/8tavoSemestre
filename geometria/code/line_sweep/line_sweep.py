c11f89f0cc9dcd3b9fda4619589906a0d025af32
DaveAlsina <davealsina@gmail.com>
2023-08-15 18:40:28 -0500

    add, a bit more on this preliminary approach to understand this

--- geometria/code/line_sweep/line_sweep.py
+++ geometria/code/line_sweep/line_sweep.py
@@ -1,8 +1,6 @@
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from typing import Union, List
-from pyparsing import abstractmethod
-from abc import ABC
+from typing import Union, List, Tuple
 
 from base import Vector, Segment
 from binary_tree import Tree, Node1D
@@ -22,35 +20,88 @@ class LineSweep:
         self.status = None # type: Tree
         self.events = None # type: List[Vector]
 
-        self.sorted_endpoints = self.sort_endpoints()
 
-    def sort_endpoints(self):
+    def sort_endpoints(self) -> List[Tuple[Vector, Segment]]:
 
         """
             Sort the endpoints of the segments.
 
             Returns:
-                list of endpoints sorted by x coordinate.
+                list of tuples (endpoint, segment) sorted by endpoint, which is a vector.
+                this sorting is done by using the vector own comparison methods.
         """
 
         endpoints = []
         for segment in self.segments:
-            endpoints.append(Node1D(segment.v0))
-            endpoints.append(Node1D(segment.v1))
+            endpoints.append((segment.start, segment))
+            endpoints.append((segment.end, segment))
 
-        tree = Tree(endpoints)
-        ordered_segments = tree.inorder()
+        # sort by using the vector own comparison methods
+        endpoints.sort(key=lambda w: w[0])
+        return endpoints
 
-        return ordered_segments
-    
+    def handle_start_endpoint(self, segment: Segment) -> bool:
+
+        """
+            Handle the start endpoint of a segment.
+            when we encounter a start endpoint, we insert the segment into the status.
+
+            Args:
+                segment: segment whose start endpoint is being handled.
+        """
+
+        if self.status is None:
+            self.status = Tree(Node1D(segment))
+        
+        else:
+            self.status.insert(Node1D(segment))
+        
+        # check for intersections, for this we need to check to the left and right of the segment
+        # in the status
+
+        #if there is a segment to the left of the current segment, and they intersect we return True
+
+        #if there is a segment to the right of the current segment, and they intersect we return True
+        
+
+    def handle_end_endpoint(self, segment: Segment) -> bool:
+
+        """
+            Handle the end endpoint of a segment.
+            when we encounter an end endpoint, we remove the segment from status, after a last check.
+
+            Args:
+                segment: segment whose end endpoint is being handled.
+        """
+
+        # if both the segment to the left and to the right exist, and they intersect, we return True
+
+        # finally we remove the segment from the status
+        self.status.remove(segment)
+
+
     def process(self) -> bool:
 
         """
-            Process the segments. And tell if there is an intersection.
+            Process the segments.
 
             Returns:
                 True if there is an intersection, False otherwise.
         """
 
-        
+        # we sort the endpoints
+        self.events = self.sort_endpoints()
 
+        # we iterate over the endpoints
+        for endpoint, segment in self.events:
+
+            # if the endpoint is the start of a segment, we handle it
+            if endpoint == segment.start:
+                self.handle_start_endpoint(segment)
+
+            # if the endpoint is the end of a segment, we handle it
+            else:
+                self.handle_end_endpoint(segment)
+
+        # if we reach this point, there are no intersections
+        return False
