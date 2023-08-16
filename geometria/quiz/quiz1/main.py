6f4eeefff4a7657bbae96a35ca02ae51726db7a4
DaveAlsina <davealsina@gmail.com>
2023-08-16 11:43:50 -0500

    add, new example for working quiz code

--- geometria/quiz/quiz1/main.py
+++ geometria/quiz/quiz1/main.py
@@ -12,7 +12,7 @@ vectors1 = [np.array([[2.2], [1.6]]), np.array([[2.0], [0.2]]),
 vectors1 = Vector.cast_to_vector(vectors1)
 
 p1 = Poligon(vectors1)
-print(f"is poligon convex? {p1.is_convex()}")
+print(f"is poligon ex. 1 convex? {p1.is_convex()}")
 plotter.plot_many(vectors1, with_labels=True)
 plotter.show()
 
@@ -24,7 +24,7 @@ vectors2 = [np.array([[1.4], [1.2]]), np.array([[2.2], [1.6]]),
 vectors2 = Vector.cast_to_vector(vectors2)
 
 p2 = Poligon(vectors2)
-print(f"is poligon convex? {p2.is_convex()}")
+print(f"is poligon ex. 2 convex? {p2.is_convex()}")
 plotter.plot_many(vectors2, with_labels=True)
 plotter.show()
 #===============================================================================
@@ -36,6 +36,21 @@ vectors3 = [np.array([[0], [0]]), np.array([[1], [0]]),
 
 vectors3 = Vector.cast_to_vector(vectors3)
 p3 = Poligon(vectors3)
-print(f"is poligon convex? {p3.is_convex()}")
+print(f"is poligon ex. 3 convex? {p3.is_convex()}")
 plotter.plot_many(vectors3, with_labels=True)
 plotter.show()
+
+#===============================================================================
+# example 4
+
+# another not convex poligon of 5 vertices
+vectors4 = [np.array([[0], [0]]), np.array([[1], [0]]),
+            np.array([[1], [1]]), np.array([[0.5], [0.5]]),
+            np.array([[0], [1]]),]
+
+vectors4 = Vector.cast_to_vector(vectors4)
+p4 = Poligon(vectors4)
+print(f"is poligon ex. 4 convex? {p4.is_convex()}")
+plotter.plot_many(vectors4, with_labels=True)
+plotter.show()
+
