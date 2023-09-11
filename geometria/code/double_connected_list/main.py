d00019a4f37e884ddb97a4e0c86f595a13991a3c
DaveAlsina <davealsina@gmail.com>
2023-09-10 20:11:52 -0500

    update, added multy subdivision plot

--- geometria/code/double_connected_list/main.py
+++ geometria/code/double_connected_list/main.py
@@ -4,11 +4,20 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from base.vector import Vector
 from double_connected_list.double_connected_segments import SemiEdgeList
+from double_connected_list.plot_double_connected_list import PlotSubdivisions 
 
-set_of_points = [Vector(np.array([[5], [1]])), Vector(np.array([[8], [3]])), 
+set_of_points1 = [Vector(np.array([[5], [1]])), Vector(np.array([[8], [3]])), 
                  Vector(np.array([[5], [-2]])), Vector(np.array([[2], [2]])), 
                  Vector(np.array([[-1], [-1]])), Vector(np.array([[1], [5]]))]
 
-list_of_semiedges = SemiEdgeList(set_of_points, name = "S1")
-print("-"*80)
-print(list_of_semiedges)
+set_of_points2 = [Vector(np.array([[0], [3]])),  Vector(np.array([[4], [3]])),
+                  Vector(np.array([[4], [-1]])), Vector(np.array([[0], [-1]]))]
+
+list_of_semiedges1 = SemiEdgeList(set_of_points1, name = "S1")
+list_of_semiedges2 = SemiEdgeList(set_of_points2, name = "S2")
+
+PlotSubdivisions.plot_many([list_of_semiedges1, list_of_semiedges2],)
+PlotSubdivisions.show()
+
+#print("-"*80)
+#print(list_of_semiedges1)
---
+++ geometria/code/double_connected_list/plot_double_connected_list.py
@@ -0,0 +1,77 @@
+from typing import List, Union
+import matplotlib.pyplot as plt
+from double_connected_segments import SemiEdgeList
+
+
+class PlotSubdivisions():
+
+    epsilon = 0.15
+    def __init__(self):
+        pass
+
+
+    @staticmethod
+    def plot(subdivision: SemiEdgeList, 
+             color: str = 'black',
+             linewidth: float = 1.0,
+             alpha: float = 1.0,
+             label: str = None) -> None:
+        """
+            This method plots a subdivision.
+        """
+
+        for semiedge in subdivision.semi_edges:
+            plt.plot([semiedge.seg.start[0], semiedge.seg.end[0]],
+                     [semiedge.seg.start[1], semiedge.seg.end[1]],
+                     color=color,
+                     linewidth=linewidth,
+                     alpha=alpha)
+        
+        plt.scatter([node.point[0] for node in subdivision.list_of_nodes],
+                    [node.point[1] for node in subdivision.list_of_nodes],
+                    color=color,
+                    alpha=alpha,)
+
+        #adds text to the points, with the geometric node name atribute 
+        for node in subdivision.list_of_nodes:
+            plt.text(node.point[0], node.point[1] + PlotSubdivisions.epsilon, f"{node.name}", 
+                     fontsize=8, color=color, alpha=alpha)
+            
+
+        plt.grid(color='gray', linestyle='--', linewidth=0.5, alpha=0.25)
+
+    def plot_many(subdivisions: List[SemiEdgeList],
+                  colors: List[str] = ['black'],
+                  linewidths: List[float] = [1.0],
+                  alphas: List[float] = [0.5],) -> None:
+
+        """
+            This method plots many subdivisions.
+        """
+
+        if len(colors) != len(subdivisions):
+            colors = colors * len(subdivisions)
+        
+        if len(linewidths) != len(subdivisions):
+            linewidths = linewidths * len(subdivisions)
+        
+        if len(alphas) != len(subdivisions):
+            alphas = alphas * len(subdivisions)
+
+
+        for i, subdivision in enumerate(subdivisions):
+            PlotSubdivisions.plot(subdivision=subdivision,
+                                  color=colors[i],
+                                  linewidth=linewidths[i],
+                                  alpha=alphas[i])
+        
+        plt.grid(color='gray', linestyle='--', linewidth=0.5, alpha=0.25)
+
+
+    @staticmethod
+    def show() -> None:
+        """
+            This method shows the plot.
+        """
+        plt.show()
+
