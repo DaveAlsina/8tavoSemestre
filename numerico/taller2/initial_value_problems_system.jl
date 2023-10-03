3daf703422f37e8418db29158cf392cb170e6b3b
DaveAlsina <davealsina@gmail.com>
2023-10-03 17:34:52 -0500

    update

---
+++ numerico/taller2/initial_value_problems_system.jl
@@ -0,0 +1,18 @@
+include("initial_value_problems.jl")
+# ----------------------------------------------------------------------- #
+#                          Systems of Equations                           #
+# ----------------------------------------------------------------------- #
+
+function RKsystem(f::Function,
+                  x0::Float64,
+                  y0::Float64,
+                  h::Float64,
+                  tol::Float64;
+                  error_type::Bool = true,
+                  parade_condition = nothing)::Tuple{Array{Float64,1},Array{Float64,1}}
+
+
+        
+
+
+end
